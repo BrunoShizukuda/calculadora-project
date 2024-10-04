866ed68c2fb4fc623cd75523ddd36e4e9409b924
Bruno Shizukuda <shizukudabruno@gmail.com>
2024-10-04 10:45:47 -0300

    feito html

---
+++ index.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelector('[data-number]')
+const operationButtons = document.querySelector('[data-operator]')
+const squalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
+
+
